f79ce591e1d9b5548c7f9fe6eca2987932a4eee6
Antonio <anikoanton@gmail.com>
2022-09-18 14:11:35 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,50 @@
+import java.sql.*;
+
+public class Main {
+
+    //отвечает за соедиенение с БД
+
+
+    static Connection postgresConnection;
+
+    public static void main(String[] args) {
+   //     DBconnect dBconnect = new DBConnect();
+
+        System.out.println("Стоп");
+       /*
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        */
+        try {
+            //DriverManager - устанавливает связь с JDBC Driver
+            //
+        //    postgresConnection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/GB", "postgres", "123")
+
+
+            postgresConnection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/GB", "postgres", "123");
+            if (!postgresConnection.equals(null))
+            {System.out.println("Connet ON");}
+            else {
+                System.out.println("Connect OFF");
+            };
+            //Statement - позволяет выполнять SQL запросы
+            Statement statement = postgresConnection.createStatement();
+            ResultSet studentsResultSet = statement.executeQuery("select * from postgres.students");
+            while (studentsResultSet.next())
+            {
+                System.out.print(studentsResultSet.getString(1));
+                System.out.print(" ");
+                System.out.print(studentsResultSet.getString("name"));
+                System.out.print(" \n");
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        String str;
+    }
+}
